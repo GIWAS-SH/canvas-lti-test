4fdc6d7c841a51e6be3deda0cd6df83468ba3fcc
GIWAS-SH <gotgzct@hotmail.com>
2026-09-08 16:32:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ const start = async () => {
     });
 
     await lti.registerPlatform({
-      url: "https://canvas.instructure.com",
+      url: "https://wisdomhouseacademy.instructure.com",
       name: "Wisdom House Academy Canvas",
       clientId: "22382000000000006",
       authenticationEndpoint:
